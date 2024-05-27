959e2195b72ea8d3df80b824918be372ea06cf34
Arthur D <arthursouzadu@gmail.com>
2024-05-27 20:05:58 -0300

    first commit

---
+++ src/main/java/com/github/dimplykhan13/reservadesala/reserva_de_sala/ReservaDeSalaApplication.java
@@ -0,0 +1,13 @@
+package com.github.dimplykhan13.reservadesala.reserva_de_sala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaDeSalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaDeSalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dimplykhan13/reservadesala/reserva_de_sala/ReservaDeSalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dimplykhan13.reservadesala.reserva_de_sala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaDeSalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
